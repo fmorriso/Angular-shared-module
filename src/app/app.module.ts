01c70669b22216f5b351dff9a00c13b057f79581
Fred Morrison <fpmorrison@verizon.net>
2017-08-02 22:00:11 -0400

    almost at contact.1b.ngmodule.zip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import {NgModule} from '@angular/core';
 
 import { AppRoutingModule }   from './app-routing.module';
 import { AppComponent }       from './app.component';
+
 import { HighlightDirective } from './highlight.directive';
+import { HighlightDirective as ContactHighlightDirective } from './contact/highlight.directive';
+
 import { TitleComponent }     from './title.component';
 import { UserService }        from './user.service';
 import { ContactComponent }   from './contact/contact.component';
 import { ContactService }     from './contact/contact.service';
 import { AwesomePipe }        from './contact/awesome.pipe';
 import { FormsModule }        from '@angular/forms';
-
 
 @NgModule({
 	imports: [
@@ -22,10 +24,11 @@ import { FormsModule }        from '@angular/forms';
 		AppComponent,
 		HighlightDirective,
 		TitleComponent,
+		AwesomePipe,
 		ContactComponent,
-		AwesomePipe
+		ContactHighlightDirective
 	],
-	providers: [UserService, ContactService],
+	providers: [ContactService, UserService],
 	bootstrap: [AppComponent]
 })
 export class AppModule {
