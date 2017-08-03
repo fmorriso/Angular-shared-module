438139804d91733220de8fa7a4a570a1dd265031
Fred Morrison <fpmorrison@verizon.net>
2017-08-03 06:42:16 -0400

    added comment about aliasing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {BrowserModule}        from '@angular/platform-browser';
+import {NgModule}             from '@angular/core';
+import { FormsModule }        from '@angular/forms';
 
 import { AppRoutingModule }   from './app-routing.module';
 import { AppComponent }       from './app.component';
 
+// use alias to distinguish between two physically different directives with the same name
 import { HighlightDirective } from './highlight.directive';
 import { HighlightDirective as ContactHighlightDirective } from './contact/highlight.directive';
 
@@ -12,7 +14,7 @@ import { UserService }        from './user.service';
 import { ContactComponent }   from './contact/contact.component';
 import { ContactService }     from './contact/contact.service';
 import { AwesomePipe }        from './contact/awesome.pipe';
-import { FormsModule }        from '@angular/forms';
+
 
 @NgModule({
 	imports: [
