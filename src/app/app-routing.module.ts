8609155c6c458269646c11a9386ce469079c35bb
Fred Morrison <fpmorrison@verizon.net>
2017-08-05 10:24:13 -0400

    added comments about failed alternative attempts and routable features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import {NgModule}             from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-//import {CrisisModule} from './crisis/crisis.module';
+import {CrisisModule} from './crisis/crisis.module';
 
+// https://github.com/johnpapa/angular-first-look-examples/tree/master/_examples/router-lazy/app
+// https://angular.io/api/router/Routes
+// ERROR in Could not resolve "crisis" from "C:/projects/Angular-shared-module/src/app/app-routing.module.ts".
 const routes: Routes = [
 	{ path: '', redirectTo: 'contact', pathMatch: 'full' },
-	{ path: 'crisis', loadChildren: './crisis/crisis.module#CrisisModule' },
+	{ path: 'crisis',
+    loadChildren: './crisis/crisis.module#CrisisModule'
+	}
 ];
 /*
 { path: 'crisis', loadChildren: './crisis/crisis.module#CrisisModule' },
